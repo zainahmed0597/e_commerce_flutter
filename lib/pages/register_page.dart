a9ad43bbee88083ac28e096f8010e59ffb16a45e
zainahmed0597 <zainahmed0597@gmail.com>
2021-04-06 11:22:48 +0500

    update UI

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,6 +6,98 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  Widget _showTitle() {
+    return Text(
+      'Register',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+
+  Widget _showUsernameInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Username',
+          hintText: 'Enter username, min length 6',
+          icon: Icon(
+            Icons.face,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Email',
+          hintText: 'Enter a valid email',
+          icon: Icon(
+            Icons.mail,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+          hintText: 'Enter password, min length 6',
+          icon: Icon(
+            Icons.lock,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _showFormActions() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        children: [
+          MaterialButton(
+              child: Text(
+                "Submit",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(color: Colors.black),
+              ),
+              elevation: 8.0,
+              onPressed: () => print("submit"),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+              color: Theme.of(context).primaryColor),
+          TextButton(
+            child: Text(
+              "Existing user? Login",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () => print("login"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,80 +113,11 @@ class _RegisterPageState extends State<RegisterPage> {
             child: Form(
               child: Column(
                 children: [
-                  Text(
-                    'Register',
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Username',
-                        hintText: 'Enter username, min length 6',
-                        icon: Icon(
-                          Icons.face,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                        hintText: 'Enter a valid email',
-                        icon: Icon(
-                          Icons.mail,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                        hintText: 'Enter password, min length 6',
-                        icon: Icon(
-                          Icons.lock,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Column(
-                      children: [
-                        MaterialButton(
-                            child: Text(
-                              "Submit",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(color: Colors.black),
-                            ),
-                            elevation: 8.0,
-                            onPressed: () => print("submit"),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),
-                            ),
-                            color: Theme.of(context).primaryColor),
-                        TextButton(
-                          child: Text("Existing user? Login"),
-                          onPressed: () => print("login"),
-                        ),
-                      ],
-                    ),
-                  ),
+                  _showTitle(),
+                  _showUsernameInput(),
+                  _showEmailInput(),
+                  _showPasswordInput(),
+                  _showFormActions(),
                 ],
               ),
             ),
